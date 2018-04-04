35d3e5afd7a9189ffd1ad22f7c9225e00cde0005
satwire <satwikowirawan@yahoo.com>
2018-04-04 15:01:57 +0200

    Initial commit

---
+++ Assets/DrawLine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour {
+
+    private LineRenderer lineRenderer;
+    private float counter;
+    private float distance;
+
+    public Transform origin;
+    public Transform destination;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
