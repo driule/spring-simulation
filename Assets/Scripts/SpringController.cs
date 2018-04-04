0df1349041e90eed0e9daa136297d1ca3077e738
Andrius Jaškauskas <jaskauskas.andrius@gmail.com>
2018-04-04 15:55:28 +0200

    sliders

--- Assets/Scripts/SpringController.cs
+++ Assets/Scripts/SpringController.cs
@@ -6,15 +6,26 @@ using UnityEngine.UI;
 public class SpringController : MonoBehaviour {
 
     public SpringJoint spring;
+    public Rigidbody load;
+    
     public Slider springCoefficientSlider;
+    public Slider loadMassSlider;
+
+    void setSpringValues()
+    {
+        this.spring.spring = this.springCoefficientSlider.value;
+        this.load.mass = this.loadMassSlider.value;
+    }
 
     // Use this for initialization
-    void Start () {
-        this.spring.spring = this.springCoefficientSlider.value;
+    void Start ()
+    {
+        this.setSpringValues();
     }
 	
 	// Update is called once per frame
-	void Update () {
-        this.spring.spring = this.springCoefficientSlider.value;
-	}
+	void Update ()
+    {
+        this.setSpringValues();
+    }
 }
