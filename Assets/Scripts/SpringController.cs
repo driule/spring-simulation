5c801d426d0cd51820f4b12faed8e34492973cbd
Andrius Jaškauskas <jaskauskas.andrius@gmail.com>
2018-04-04 15:31:51 +0200

    Spring joint controller

---
+++ Assets/Scripts/SpringController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpringController : MonoBehaviour {
+
+    public SpringJoint spring;
+    public Slider springCoefficientSlider;
+
+    // Use this for initialization
+    void Start () {
+        this.spring.spring = this.springCoefficientSlider.value;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.spring.spring = this.springCoefficientSlider.value;
+	}
+}
