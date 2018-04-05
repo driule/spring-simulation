03196ce4c446370b6fe0aa23a3d13ff8ddde1cef
Andrius Jaškauskas <jaskauskas.andrius@gmail.com>
2018-04-05 16:34:29 +0200

    fluid controller created

---
+++ Assets/Scripts/FluidController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Obi;
+
+public class FluidController : MonoBehaviour {
+
+    public ObiEmitterMaterialFluid fluidMaterial;
+    public ObiEmitter fluidEmmiter;
+
+    public Slider fluidViscositySlider;
+
+    // Use this for initialization
+    void Start () {
+        this.setFluidValues();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.setFluidValues();
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            this.fluidEmmiter.KillAll();
+        }
+    }
+
+    void setFluidValues()
+    {
+        this.fluidMaterial.viscosity = this.fluidViscositySlider.value;
+    }
+}
